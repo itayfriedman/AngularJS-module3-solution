7926cf8f164493e1f4dee6d35bb99efc763f4a40
itayfriedman <itay.friedman@mail.huji.ac.il>
2017-03-08 15:24:33 +0200

    start solution 1.2

--- app.js
+++ app.js
@@ -4,7 +4,32 @@
   angular.module('NarrowItDownApp', [])
 
   .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService);
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemDirective)
+
+  function FoundItemDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      // items: '<',
+      // myTitle: '@title',
+      onRemove: '&'
+    },
+    controller: FoundItemDirectiveController,
+    controllerAs: 'list',
+    bindToController: true,
+  };
+
+  return ddo;
+}
+
+function FoundItemDirectiveController() {
+  var list = this;
+
+  list.isEmpty = function () {
+    return list.found.length ===0;
+  }
+}
 
 MenuSearchService.$inject = ['$http']
 function MenuSearchService($http) {
@@ -13,13 +38,16 @@ function MenuSearchService($http) {
 service.getMatchedMenuItems = function(searchTerm) {
   var response  =  $http({
     method: "GET",
-    url: (""),
-    params: {
-
-    }
+    url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
   });
 
-  return response;
+  // return $http(...).then(function (result) {
+  //     // process result and only keep items that match
+  //     var foundItems...
+  //
+  //     // return processed items
+  //     return foundItems;
+  // });
 };
 
 }
