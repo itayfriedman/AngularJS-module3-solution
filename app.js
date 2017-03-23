9ec72d9a4776ce0869023339b0528b9cb91e79d7
itayfriedman <itay.friedman@mail.huji.ac.il>
2017-03-24 00:37:50 +0200

    clean with httpworking

--- app.js
+++ app.js
@@ -5,57 +5,35 @@
 
   .controller('NarrowItDownController', NarrowItDownController)
   .service('MenuSearchService', MenuSearchService)
-  .directive('foundItems', FoundItemDirective)
+  // .directive('foundItems', FoundItemDirective)
 
-  function FoundItemDirective() {
-console.log("FoundItemDirective");
-  var ddo = {
-    templateUrl: 'foundItems.html',
-    scope: {
-      items: '<',
-      onRemove: '&'
-    },
-    controller: FoundItemDirectiveController,
-    controllerAs: 'list',
-    bindToController: true,
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var items = this;
+
+    var promise = MenuSearchService.getMatchedMenuItems();
+
+    promise.then(function (response) {
+
+      items.founds = response.data.menu_items;
+    })
+    .catch(function (error) {
+      console.log("Someting went worng !");
+    });
+  }
+
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http) {
+    var service = this;
+
+    service.getMatchedMenuItems = function () {
+    var response = $http({
+      method: "GET",
+      url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+    });
+
+    return response;
   };
-
-  return ddo;
 }
 
-function FoundItemDirectiveController() {
-  var list = this;
-
-  list.isEmpty = function () {
-    return list.found.length ===0;
-  }
-}
-
-MenuSearchService.$inject = ['$http']
-function MenuSearchService($http) {
-  var service = this;
-
-service.getMatchedMenuItems = function(searchTerm) {
-console.log("getMatchedMenuItems");
-  return  $http({
-    method: "GET",
-    url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-  }).then(function(response) {
-    var foundItems = response.data;
-    console.log("foundItems  ", foundItems);
-    
-  });
-  }
-
-
-  // return $http(...).then(function (result) {
-  //     // process result and only keep items that match
-  //     var foundItems...
-  //
-  //     // return processed items
-  //     return foundItems;
-  // });
-};
-
-}
 })();
