cce7c1b43393536bc65c8cd41d94da622d709ff6
itayfriedman <itay.friedman@mail.huji.ac.il>
2017-03-26 08:18:36 +0300

    working on the dirictive

--- app.js
+++ app.js
@@ -5,34 +5,78 @@
 
   .controller('NarrowItDownController', NarrowItDownController)
   .service('MenuSearchService', MenuSearchService)
-  // .directive('foundItems', FoundItemDirective)
+  .directive('foundItems', FoundItemDirective)
+
+  function FoundItemDirective() {
+
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        found: '<',
+        onRemove: '&'
+      },
+      controller: FoundItemDirectiveController,
+      controllerAs: 'items',
+      bindToController: true,
+    };
+
+    return ddo;
+  }
+
+  function FoundItemDirectiveController() {
+    var found = this;
+    if (found.length = 0 ) {
+      return true;
+    }
+
+  }
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
     var items = this;
+    items.searchItem = "";
 
-    var promise = MenuSearchService.getMatchedMenuItems();
+  items.nerrowIt = function () {
+    if (items.searchItem === ""){
+      items.founds =[];
+      return;
+    }
 
+    var promise = MenuSearchService.getMatchedMenuItems(items.searchItem);
     promise.then(function (response) {
-
-      items.founds = response.data.menu_items;
+      items.founds = response;
     })
     .catch(function (error) {
       console.log("Someting went worng !");
     });
   }
 
+  }
+
   MenuSearchService.$inject = ['$http'];
   function MenuSearchService($http) {
     var service = this;
 
-    service.getMatchedMenuItems = function () {
-    var response = $http({
+    service.getMatchedMenuItems = function (searchItem) {
+    return $http({
       method: "GET",
       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-    });
+    }).then(function (result) {
 
-    return response;
+      var items = result.data.menu_items;
+      var foundItems = [];
+      // var searchItem = "Egg";
+
+            for (var i = 0; i < items.length; i++) {
+              if (items[i].description.toLowerCase().indexOf(searchItem.toLowerCase()) >=0 ){
+
+                foundItems.push(items[i]);
+              }
+            }
+            // return processed items
+      return foundItems;
+
+    })
   };
 }
 
